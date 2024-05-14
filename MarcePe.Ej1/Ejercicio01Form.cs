a79d9d190fb0c10c69e3cdaf773c3f2448451db7
marcelopele <marcelopele@example.com>
2024-05-14 20:36:47 -0300

    Primer commit

---
+++ MarcePe.Ej1/Ejercicio01Form.cs
@@ -0,0 +1,13 @@
+namespace MarcePe.Ej1
+{
+    public partial class Ejercicio01Form : Form
+    {
+        private readonly Ejercicio01Model modelo= new Ejercicio01Model();
+
+        public Ejercicio01Form()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ MarcePe.Ej1/Ejercicio01Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarcePe.Ej1
+{
+    internal class Ejercicio01Model
+    {
+    }
+}
